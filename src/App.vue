f9b20bd06f35fe345448659225da26f6c7beee5e
Akira Kuroiwa <akira.kuroiwa@dena.com>
2019-05-22 10:20:53 +0900

    add game logics

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="whackamole">
     <h1 class="logo">Whack-a-mole!</h1>
-    <button class="start-game">Start Game</button>
+    <button class="start-game" v-on:click="startGame">Start Game</button>
     <div class="counters-container">
       <Counter label="Score:" v-bind:value="score"/>
       <Counter label="High Score:" v-bind:value="highScore"/>
@@ -26,9 +26,39 @@ export default {
       score: 0,
       highScore: 0,
       timer: 20,
-      moles: [true, false, true, false],
+      moles: [false, false, false, false],
       gameActive: false
     };
+  },
+  methods: {
+    resetState: function() {
+      this.score = 0;
+      this.timer = 20;
+      this.moles = [false, false, false, false];
+    },
+    startGame: function() {
+      this.resetState();
+      this.gameActive = true;
+      this.startTimer();
+    },
+    endGame: function() {
+      this.gameActive = false;
+      this.stopTimer();
+    },
+    startTimer: function() {
+      this.timerId = setInterval(() => {
+        this.decrementTime();
+      }, 1000);
+    },
+    decrementTime: function() {
+      this.timer--;
+      if (this.timer === 0) {
+        this.endGame();
+      }
+    },
+    stopTimer: function() {
+      clearInterval(this.timerId);
+    }
   }
 };
 </script>
